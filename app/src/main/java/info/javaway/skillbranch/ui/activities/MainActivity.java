0f58e1ab7a11436f8dc5bbd135b02466c0ce78ae
PikselNsk <PikselNsk@gmail.com>
2017-03-16 19:31:02 +0300

    Initial commit

---
+++ app/src/main/java/info/javaway/skillbranch/ui/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package info.javaway.skillbranch.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import info.javaway.skillbranch.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
