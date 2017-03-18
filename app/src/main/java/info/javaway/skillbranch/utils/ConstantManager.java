05a5d76ad555cee2c4da638ed557b007f431f4f5
PikselNsk <PikselNsk@gmail.com>
2017-03-18 15:00:12 +0300

    Edit layout

---
+++ app/src/main/java/info/javaway/skillbranch/utils/ConstantManager.java
@@ -0,0 +1,10 @@
+package info.javaway.skillbranch.utils;
+
+/**
+ * Created by максим on 17.03.2017.
+ */
+
+public interface ConstantManager {
+    String TAG_PREFIX = "DEV ";
+    String COLOR_MODE_KEY = "COLOR_MODE_KEY";
+}
