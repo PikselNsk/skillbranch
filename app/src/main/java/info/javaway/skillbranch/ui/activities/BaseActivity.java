687ebd3995ba705a230057eea3426ac4a30db1d6
PikselNsk <PikselNsk@gmail.com>
2017-03-19 17:38:24 +0300

    Add showWIthDelay method

--- app/src/main/java/info/javaway/skillbranch/ui/activities/BaseActivity.java
+++ app/src/main/java/info/javaway/skillbranch/ui/activities/BaseActivity.java
@@ -1,10 +1,14 @@
 package info.javaway.skillbranch.ui.activities;
 
 import android.app.ProgressDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
 
+import info.javaway.skillbranch.R;
 import info.javaway.skillbranch.utils.ConstantManager;
 
 /**
@@ -16,20 +20,41 @@ public class BaseActivity extends AppCompatActivity {
     static final String TAG = ConstantManager.TAG_PREFIX + "BaseActivity";
     protected ProgressDialog mProgressDialog;
 
-    public void showProgress(){
-        if (mProgressDialog == null){
-            mProgressDialog = new ProgressDialog(this, )
+    public void showProgress() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this, R.style.custom_dialog);
+            mProgressDialog.setCancelable(false);
+            mProgressDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            mProgressDialog.show();
+            mProgressDialog.setContentView(R.layout.progress_splash);
+        } else {
+            mProgressDialog.show();
+            mProgressDialog.setContentView(R.layout.progress_splash);
         }
     }
 
-    public void hideProgress(){}
+    public void hideProgress() {
+        if ((mProgressDialog != null) && mProgressDialog.isShowing()) {
+            mProgressDialog.hide();
+        }
+    }
 
-    public void showError(String message, Exception error){
+    public void showWithDelay() {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                hideProgress();
+            }
+        }, 5000);
+    }
+
+    public void showError(String message, Exception error) {
         showTost(message);
         Log.d(TAG, error.toString());
     }
 
-    public void showTost(String message){
+    public void showTost(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/info/javaway/skillbranch/ui/activities/MainActivity.java
+++ app/src/main/java/info/javaway/skillbranch/ui/activities/MainActivity.java
@@ -1,22 +1,32 @@
 package info.javaway.skillbranch.ui.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import info.javaway.skillbranch.R;
 import info.javaway.skillbranch.utils.ConstantManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     public static final String  TAG = ConstantManager.TAG_PREFIX + "MainActivity.TAG";
+    private ImageView mAboutImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate");
-
+        mAboutImageView = (ImageView) findViewById(R.id.about_button);
+        mAboutImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showProgress();
+            }
+        });
         if (savedInstanceState == null){
             //активити создается впервые
         }else{
